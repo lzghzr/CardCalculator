9c1bf81cb1550d03dd7ab2085be5b8662011db00
lzghzr <lzggzr@gmail.com>
2015-11-24 01:48:00 +0800

    初版

---
+++ CardCalculator/Calculator.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace CardCalculator
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labLowestPrice = new System.Windows.Forms.Label();
+            this.labExchangeRate = new System.Windows.Forms.Label();
+            this.texLowestPrice = new System.Windows.Forms.TextBox();
+            this.texExchangeRate = new System.Windows.Forms.TextBox();
+            this.texResult = new System.Windows.Forms.TextBox();
+            this.labResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labLowestPrice
+            // 
+            this.labLowestPrice.AutoSize = true;
+            this.labLowestPrice.Location = new System.Drawing.Point(15, 15);
+            this.labLowestPrice.Name = "labLowestPrice";
+            this.labLowestPrice.Size = new System.Drawing.Size(77, 12);
+            this.labLowestPrice.TabIndex = 0;
+            this.labLowestPrice.Text = "市场最低价格";
+            // 
+            // labExchangeRate
+            // 
+            this.labExchangeRate.AutoSize = true;
+            this.labExchangeRate.Location = new System.Drawing.Point(15, 45);
+            this.labExchangeRate.Name = "labExchangeRate";
+            this.labExchangeRate.Size = new System.Drawing.Size(77, 12);
+            this.labExchangeRate.TabIndex = 2;
+            this.labExchangeRate.Text = "当前美元汇率";
+            // 
+            // texLowestPrice
+            // 
+            this.texLowestPrice.AllowDrop = true;
+            this.texLowestPrice.Location = new System.Drawing.Point(165, 10);
+            this.texLowestPrice.MaxLength = 7;
+            this.texLowestPrice.Name = "texLowestPrice";
+            this.texLowestPrice.Size = new System.Drawing.Size(100, 21);
+            this.texLowestPrice.TabIndex = 1;
+            this.texLowestPrice.KeyUp += new System.Windows.Forms.KeyEventHandler(this.texLowestPrice_KeyUp);
+            // 
+            // texExchangeRate
+            // 
+            this.texExchangeRate.AllowDrop = true;
+            this.texExchangeRate.Location = new System.Drawing.Point(165, 40);
+            this.texExchangeRate.MaxLength = 7;
+            this.texExchangeRate.Name = "texExchangeRate";
+            this.texExchangeRate.Size = new System.Drawing.Size(100, 21);
+            this.texExchangeRate.TabIndex = 3;
+            this.texExchangeRate.Text = "6.37";
+            this.texExchangeRate.KeyUp += new System.Windows.Forms.KeyEventHandler(this.texExchangeRate_KeyUp);
+            // 
+            // texResult
+            // 
+            this.texResult.Location = new System.Drawing.Point(165, 70);
+            this.texResult.MaxLength = 7;
+            this.texResult.Name = "texResult";
+            this.texResult.ReadOnly = true;
+            this.texResult.Size = new System.Drawing.Size(100, 21);
+            this.texResult.TabIndex = 6;
+            // 
+            // labResult
+            // 
+            this.labResult.AutoSize = true;
+            this.labResult.Location = new System.Drawing.Point(15, 75);
+            this.labResult.Name = "labResult";
+            this.labResult.Size = new System.Drawing.Size(77, 12);
+            this.labResult.TabIndex = 5;
+            this.labResult.Text = "建议收款价格";
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 111);
+            this.Controls.Add(this.labResult);
+            this.Controls.Add(this.texResult);
+            this.Controls.Add(this.texExchangeRate);
+            this.Controls.Add(this.texLowestPrice);
+            this.Controls.Add(this.labExchangeRate);
+            this.Controls.Add(this.labLowestPrice);
+            this.Name = "Calculator";
+            this.Text = "CardCalculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labLowestPrice;
+        private System.Windows.Forms.Label labExchangeRate;
+        private System.Windows.Forms.TextBox texLowestPrice;
+        private System.Windows.Forms.TextBox texExchangeRate;
+        private System.Windows.Forms.TextBox texResult;
+        private System.Windows.Forms.Label labResult;
+    }
+}
+
---
+++ CardCalculator/Calculator.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CardCalculator
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void texLowestPrice_KeyUp(object sender, KeyEventArgs e)
+        {
+            Calculate();
+        }
+
+        private void texExchangeRate_KeyUp(object sender, KeyEventArgs e)
+        {
+            Calculate();
+        }
+
+        private void Calculate()
+        {
+            if (texLowestPrice.Text != "" && double.Parse(texLowestPrice.Text) >= 0.03 && texExchangeRate.Text != "")
+            {
+                double Amount_d = double.Parse(texLowestPrice.Text);
+                double ExchangeRate_d = double.Parse(texExchangeRate.Text);
+                Amount_d = CalculateAmount(Amount_d, ExchangeRate_d);
+                texResult.Text = Amount_d.ToString();
+            }
+            else
+            {
+                texResult.Text = "error";
+            }
+        }
+
+        private double CalculateAmount(double Amount, double ExchangeRate)
+        {
+            double Amount_d = Math.Floor(Amount * 100);
+            double EstimatedAmountOfWalletFundsReceivedByOtherParty_d = Math.Floor(Amount * 100 / 1.15);
+            double[] Amount_da = new double[2];
+            Amount_da = CalculateAmountToSendForDesiredReceivedAmount(EstimatedAmountOfWalletFundsReceivedByOtherParty_d);
+            int i = 0;
+            bool EverUndershot = false;
+            while (Amount_da[1] != Amount_d && i < 10)
+            {
+                if (Amount_da[1] > Amount_d)
+                {
+                    if (EverUndershot)
+                    {
+                        Amount_da = CalculateAmountToSendForDesiredReceivedAmount(EstimatedAmountOfWalletFundsReceivedByOtherParty_d - 1);
+                        Amount_da[0] += (Amount_d - Amount_da[1]);
+                        Amount_da[1] += Amount_d;
+                        break;
+                    }
+                    else
+                    {
+                        EstimatedAmountOfWalletFundsReceivedByOtherParty_d--;
+                    }
+                }
+                else
+                {
+                    EverUndershot = true;
+                    EstimatedAmountOfWalletFundsReceivedByOtherParty_d++;
+                }
+                Amount_da = CalculateAmountToSendForDesiredReceivedAmount(EstimatedAmountOfWalletFundsReceivedByOtherParty_d);
+                i++;
+            }
+            double uAmount_d = (Amount_d - Amount_da[0]) / ExchangeRate;
+            uAmount_d = Math.Ceiling(uAmount_d);
+            Amount = Math.Floor(uAmount_d * ExchangeRate) / 100;
+            return Amount;
+        }
+
+        private double[] CalculateAmountToSendForDesiredReceivedAmount(double receivedAmount)
+        {
+            double[] Amount = new double[2];
+            double SteamFee_d = receivedAmount * 0.05;
+            if (SteamFee_d > 1)
+            {
+                SteamFee_d = Math.Floor(SteamFee_d);
+            }
+            else
+            {
+                SteamFee_d = 1;
+            }
+            double PublisherFee_d = receivedAmount * 0.10;
+            if (PublisherFee_d > 1)
+            {
+                PublisherFee_d = Math.Floor(PublisherFee_d);
+            }
+            else
+            {
+                PublisherFee_d = 1;
+            }
+            Amount[0] = SteamFee_d + PublisherFee_d;
+            Amount[1] = receivedAmount + SteamFee_d + PublisherFee_d;
+            return Amount;
+        }
+    }
+}
